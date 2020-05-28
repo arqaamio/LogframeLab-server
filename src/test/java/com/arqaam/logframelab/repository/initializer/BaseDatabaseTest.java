96e51650d227bb51ff5bfe39f3045238547ea3c4
Dotun <dotunkolaolaleye@gmail.com>
2020-05-28 13:35:57 +0200

    [#5jnexr]
    Using Testcontainers to deploy MySQL instance on docker for tests.

---
+++ src/test/java/com/arqaam/logframelab/repository/initializer/BaseDatabaseTest.java
@@ -0,0 +1,40 @@
+package com.arqaam.logframelab.repository.initializer;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@ActiveProfiles("integration")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = BaseDatabaseTest.Initializer.class)
+@Testcontainers
+public interface BaseDatabaseTest {
+
+  @Container
+  MySQLContainer<?> mySQLContainer =
+      new MySQLContainer<>("mysql:5.7")
+          .withDatabaseName("integration_tests")
+          .withUsername("root")
+          .withPassword("")
+          .withPrivilegedMode(true);
+
+  @TestPropertySource(locations = "classpath:application-integration.properties")
+  class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
+      TestPropertyValues.of(
+          "spring.datasource.url=" + mySQLContainer.getJdbcUrl(),
+          "spring.flyway.url=" + mySQLContainer.getJdbcUrl())
+          .applyTo(applicationContext.getEnvironment());
+    }
+  }
+}

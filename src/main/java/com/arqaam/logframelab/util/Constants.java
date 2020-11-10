6b485f2cee4f23ecdc136d906e082ec99d3cbc01
Ari <61361948+git-ari@users.noreply.github.com>
2020-11-10 15:36:38 +0000

    [#a4u08x] - Allow search for empty values

--- src/main/java/com/arqaam/logframelab/util/Constants.java
+++ src/main/java/com/arqaam/logframelab/util/Constants.java
@@ -9,4 +9,7 @@ public class Constants {
 
     public static final String WORD_FILE_EXTENSION = ".docx";
     public static final String WORKSHEET_FILE_EXTENSION = ".xlsx";
+
+    public static final String EMPTY_VALUE = "empty_string";
+    public static final Long EMPTY_VALUE_ID = -1L;
 }
